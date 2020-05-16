8e526dd67800fc79d06b797dfcec063cfb34f8db
Ioannis Tziokas (DHL IT Services) <Ioannis.Tziokas@dhl.com>
2020-05-16 11:52:41 +0200

    image in scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,11 @@ void main() {
           title: Text('ESS & ESI'),
           backgroundColor: Colors.blueGrey[900],
         ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
