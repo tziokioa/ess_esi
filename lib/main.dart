ef8940bf3bbb79ede1bd7339ffc6f76f632eb39a
Ioannis Tziokas (DHL IT Services) <Ioannis.Tziokas@dhl.com>
2020-05-16 11:39:51 +0200

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'ESS & ESI',
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('ESS & ESI'),
+          backgroundColor: Colors.blueGrey[900],
+          ),
         ),
       ),
-    ),
   );
 }
